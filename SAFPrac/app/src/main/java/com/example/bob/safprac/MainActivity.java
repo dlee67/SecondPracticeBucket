8c704136789749a7b37640b6b5f9e1c97d765b14
dlee67 <dlee67@msudenver.edu>
2018-07-22 19:09:51 -0600

    At this point, it is likely that I am using the Output/InputStream wrong.

--- SAFPrac/app/src/main/java/com/example/bob/safprac/MainActivity.java
+++ SAFPrac/app/src/main/java/com/example/bob/safprac/MainActivity.java
@@ -14,7 +14,11 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -60,10 +64,14 @@ public class MainActivity extends AppCompatActivity {
         } else{
             populateImages();
 
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            //Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("*/*");
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
             startActivityForResult(intent, READ_REQUEST_CODE);
+            //startActivityForResult(intent, READ_REQUEST_CODE);
         }
     }
 
@@ -71,12 +79,12 @@ public class MainActivity extends AppCompatActivity {
         imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         Log.i("dhl", "Fetching directory: " + imagesDir.getAbsolutePath());
         listOfFiles = imagesDir.list();
-
         if(listOfFiles == null){
             Log.i("dhl", "List of files are null. Returning out of the method.");
             return;
         }
 
+        images = imagesDir.listFiles();
         for(File str : images) {
             listOfImagesUri.add(Uri.fromFile(str));
         }
@@ -84,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult (int requestCode, int resultCode, Intent data){
-        usbRoot = fromTreeUri(this, data.getData());
+        //usbRoot = fromTreeUri(this, data.getData());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             grantUriPermission(this.getPackageName(), data.getData(),
                     Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
@@ -93,7 +101,28 @@ public class MainActivity extends AppCompatActivity {
             getContentResolver().takePersistableUriPermission(data.getData(),
                     Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         }
-        writeToTextFile();
+
+        PrintWriter pw = null;
+        //Create an output stream, which writes text files.
+        try {
+            Log.i("dhl", "Carrying out the alternative.");
+            pw = new PrintWriter(getContentResolver()
+                    .openOutputStream
+                    //(baseUri.withAppendedPath(baseUri, "some_text.txt")));
+                            (data.getData()));
+        } catch (Exception e) {
+            Log.i("dhl", e.toString());
+            return;
+        }
+
+        pw.write("In regards to institution we criticize.");
+//https://stackoverflow.com/questions/42043114/how-to-write-to-documentfile-in-android-programmatically
+        pw.flush();
+        pw.close();
+
+        //Log.i("dhl", "Calling writeToTextFile()");
+        //writeToTextFile();
+        //writeImageFiles();
     }
 
     /*
@@ -113,35 +142,55 @@ public class MainActivity extends AppCompatActivity {
         Log.i("dhl", "Current Uri can: " + usbRoot.canRead());
         Log.i("dhl", "Current Uri can: " + usbRoot.canWrite());
         Uri baseUri = usbRoot.getUri();
+        Log.i("dhl", "Uri of the usbRoot at the moment: " + usbRoot.getUri().getPath());
         usbRoot.createFile("text/plain", "some_text.txt");
+        DocumentFile textFileInSomeFolder = usbRoot.listFiles()[0];
         PrintWriter pw = null;
         //Create an output stream, which writes text files.
         try {
             pw = new PrintWriter(getContentResolver()
                     .openOutputStream
-                            (baseUri.withAppendedPath(baseUri, "./some_text.txt")));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+                            //(baseUri.withAppendedPath(baseUri, "some_text.txt")));
+                                    (textFileInSomeFolder.getUri()));
+        } catch (Exception e) {
+            Log.i("dhl", e.toString());
+            return;
         }
         pw.write("In regards to institution we criticize.");
+//https://stackoverflow.com/questions/42043114/how-to-write-to-documentfile-in-android-programmatically
+        pw.flush();
         pw.close();
     }
 
     //I have to film a Shaolin movie with DocumentFile with this one.
-    public void writeImageFiles(){
-
-        //ArrayList<OutputStream> listOfOutputStream = new ArrayList<OutputStream>();
-        for(int counter = 0; counter < listOfImagesUri.size(); counter++){
-            try {
-                //I have to be clever about the below, and somehow chuck in ImageFiles
-                //into the USB.
-                getContentResolver().openOutputStream(listOfImagesUri.get(counter));
-            } catch (FileNotFoundException e) {
-                Log.i("dhl", e.toString());
-                return;
+    public void writeImageFiles() {
+        //If the usbRoot doesn't have an Uri path to the ./bug_report, then make it so.
+        //Uri imgDirUri = Uri.withAppendedPath(usbRoot.getUri(), "./bug_report");
+        try {
+            /*
+            Just like the things I've learned about in the OS course, the below block of code
+            should be able to stream the data from the intended directory from OutPutStream
+            to InputStream.
+             */
+            usbRoot.createFile("image/png", "some_image");
+            Uri uriToImg = Uri.withAppendedPath(usbRoot.getUri(), "some_image.png");
+            Log.i("dhl", "uriToImg's uri is: " + uriToImg.getPath());
+            OutputStream out = getContentResolver().openOutputStream(uriToImg);
+            InputStream in = getContentResolver().openInputStream(listOfImagesUri.get(0)); //Grabs the very first element of the listOfImagesUri.
+            Log.i("dhl", "From listOfImagesUri: " + listOfImagesUri.get(0).toString());
+            byte[] buffers = new byte[1024];
+            int read = 0;
+            while((read = in.read(buffers)) != -1){
+                out.write(buffers, 0, read);
             }
+        } catch (FileNotFoundException e) {
+            Log.i("dhl", e.toString());
+            return;
+        } catch (IOException e) {
+            Log.i("dhl", e.toString());
+            return;
         }
-
+        //ArrayList<OutputStream> listOfOutputStream = new ArrayList<OutputStream>();
     }
 
     public void onRequestPermissionsResult(int requestCode,
