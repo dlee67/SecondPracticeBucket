d0c6d3260b160d22f47e9c14aacb6e3701092385
dlee67 <dlee67@msudenver.edu>
2018-08-18 23:52:25 -0600

    Today's batch.

--- AboutFragments/app/src/main/java/com/example/bob/aboutfragments/ContentFragment.java
+++ AboutFragments/app/src/main/java/com/example/bob/aboutfragments/ContentFragment.java
@@ -12,6 +12,6 @@ public class ContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                 Bundle savedInstance){
-        return inflater.inflate(R.layout.mainfrag, container, false);
+        return inflater.inflate(R.layout.mainfrag, container, false);   
     }
 }
--- GoPractice/WhatMakesCSpecial.go
+++ GoPractice/WhatMakesCSpecial.go
@@ -27,6 +27,22 @@ func main(){
    someStudent.name = "Lulz!"
    someStudent.GPA = 9000.0
    someStudent.age = 16
+   fmt.Println("Student's name: ", someStudent.name)
+   fmt.Println("Student's GPA: ", someStudent.GPA)
+   fmt.Println("Student's age: ", someStudent.age)
+   setStudent(someStudent)
+   fmt.Println("Student's name: ", someStudent.name)
+   fmt.Println("Student's GPA: ", someStudent.GPA)
+   fmt.Println("Student's age: ", someStudent.age)
+
+   var otherStudent Student = Student{name: "You!", GPA: 4.0, age: 20}
+   //Regarding the line above,
+   //var otherStudent *Student = Student{name: "You!", GPA: 4.0, age: 20} //The left doesn't work...
+   fmt.Println("Your name: ", otherStudent.name)
+   fmt.Println("Your GPA: ", otherStudent.GPA)
+   fmt.Println("Your age: ", otherStudent.age)
+
+   fmt.Println("This is how receiver works: ", otherStudent.getAge())
 }
 
 func passByValue(x int){
@@ -40,4 +56,18 @@ func passByReference(x *int){
 
 func setInt(y *int) {
    *y = 10 //Dereference it again to mutate it.
+}
+
+func setStudent(someStudent *Student){
+   someStudent.name = "Not so funny"
+   someStudent.GPA = 0.0
+   someStudent.age = 15
+}
+
+//There is this thing called receiver, and they are defined outside the lexical context of the structs;
+//however, it's suppose to immitate a method from OOP capable languages.
+//It does, but it's really hard to read...
+//so, if I am invoking getAge() like: someStudent.getAge(), I am getting the Age of the someStudent.
+func (s *Student) getAge() int{
+   return s.age
 }
